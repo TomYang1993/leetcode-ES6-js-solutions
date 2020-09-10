c0527cca69945b9de904dcf2fe7ac60d0806b116
Tom Yang <xy2286@columbia.edu>
2020-09-10 10:55:28 -0400

    binary search tree

--- remove-duplicates-string-number.js
+++ remove-duplicates-string-number.js
@@ -22,22 +22,6 @@ function removeDuplicatesByIndexOfOptimized(arr) {
     return resultArray;
 }
 
-
-function distinct(a, b) {
-    let arr = a.concat(b)
-    let result = []
-    let obj = {}
-
-    for (let i of arr) {
-        if (!obj[i]) {
-            result.push(i)
-            obj[i] = 1
-        }
-    }
-
-    return result
-}
-
 function removeDuplicatesByMap(arr) {
     if (!Array.isArray(arr)) {
         console.log('type error!')
